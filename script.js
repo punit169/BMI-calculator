84e770c2acdbd06daa566b0e55aa11ad3e08fc57
punit169 <147256719+punit169@users.noreply.github.com>
2024-09-29 08:23:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+/*
+    in form first submit prevent func(which refresh page) excecuted so we cant use onclick
+    preventdefault() will prevent page from refresh it prevents from default func
+
+*/
+document.getElementById("bmi-form").addEventListener("submit", function (e) {
+
+    e.preventDefault();
+    const Gender = document.getElementById("Gender").value;
+    const Age = parseInt(document.getElementById("Age").value);
+    const Feet = parseInt(document.getElementById("Feet").value);
+    const Inches = parseInt(document.getElementById("Inches").value);
+    const Weight = parseFloat(document.getElementById("Weight").value);
+
+    const HeightInMeter = (((Feet * 12) + Inches) * 0.0254);
+    const bmi = Weight / (HeightInMeter * HeightInMeter);
+    const result = document.getElementById("result");
+
+    let category = '';
+
+    if (bmi < 18.5) {
+        category = 'Under Weight';
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        category = 'Normal Weight';
+    } else if (bmi >= 25 && bmi < 29.9) {
+        category = 'Over Weight'
+    } else {
+        category = 'Obese'
+    }
+
+    let resultmassage = 'Your BMI:' + bmi.toFixed(2) + '<br>';
+    resultmassage += 'Category:' + category;
+
+    result.innerHTML = resultmassage;
+
+
+
+
+});
